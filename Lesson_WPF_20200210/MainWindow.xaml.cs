d2308a0ea01135b32b2cc7a9162d6d62f3798d87
VladimirOrlov <Vladimir.Orlov.A@mail.ru>
2020-02-10 20:43:42 +0600

    Добавьте файлы проекта.

---
+++ Lesson_WPF_20200210/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_WPF_20200210
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabControl tabControl = (TabControl)sender;
+            TabItem tabItem = tabControl.SelectedItem as TabItem;
+
+            switch(tabItem.Header)
+            {
+                case "Tab item3":
+                    MessageBox.Show("Work tab3");
+                    break;
+            }
+        }
+    }
+}
